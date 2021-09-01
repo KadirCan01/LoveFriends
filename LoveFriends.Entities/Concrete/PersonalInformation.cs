240479d51a23a2bff6ad0c2e919e46e969b3d44b
Kadir <Kadir@DESKTOP-BB6RLTO>
2021-09-01 19:49:37 +0300

    Add project files.

---
+++ LoveFriends.Entities/Concrete/PersonalInformation.cs
@@ -0,0 +1,21 @@
+﻿using LoveFriends.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoveFriends.Entities.Concrete
+{
+    public class PersonalInformation:EntityBase, IEntity
+
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber  { get; set; }
+        public string  Mail { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+    }
+}
---
+++ LoveFriends.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoveFriends.Shared.Entities.Abstract
+{
+   public abstract class EntityBase
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public bool IsActive  { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
